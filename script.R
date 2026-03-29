98b4ab54d17eb5ad012c823002f1ca78531d5f2a
PenpichaCode <penpicha.kog2@gemail.com>
2026-03-29 17:44:01 +0000

    Drop Na on Drug & Imputation mean all column with mean

--- script.R
+++ script.R
@@ -1,15 +1,37 @@
 library(dplyr)
 library(tidyverse)
 library(readr)
+library(ggplot2)
 
 # import data set.
 cirrhosis <- read_csv("cirrhosis.csv")
 View(cirrhosis)
+
+## data preparing
+
+# check Normal Distribution
+ggplot(cirrhosis, aes(Age)) +
+  geom_histogram(fill = "steelblue", color = "white") +
+  theme_minimal()
 
 # check NA / missing value.
 colSums(is.na(cirrhosis))
 
 # review and summary the data.
 glimpse(cirrhosis)
-summary(cirrhosis)
+
+# drop only the row where the drug column is NA
+cirrhosis_clean <- cirrhosis %>% 
+  filter(!is.na(Drug))
+view(cirrhosis_clean)
+
+# Imputed missing values with Mean all of numeric
+cirrhosis_imputed <- cirrhosis_clean %>% 
+  mutate(across(where(is.numeric), ~replace_na(., mean(., na.rm = TRUE))))
+
+
+
+
+
+
 
