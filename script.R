cdc15bf78efc17f292461faacd62b4f4662c3a6b
PenpichaCode <penpicha.kog2@gemail.com>
2026-03-29 14:27:36 +0000

    check Na/missing

--- script.R
+++ script.R
@@ -2,6 +2,14 @@ library(dplyr)
 library(tidyverse)
 library(readr)
 
-# import dataset
+# import data set.
 cirrhosis <- read_csv("cirrhosis.csv")
 View(cirrhosis)
+
+# check NA / missing value.
+colSums(is.na(cirrhosis))
+
+# review and summary the data.
+glimpse(cirrhosis)
+summary(cirrhosis)
+
