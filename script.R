74c9963522909f04995ff37b6f5ada9ed884cd12
PenpichaCode <penpicha.kog2@gemail.com>
2026-03-29 18:20:02 +0000

    Clean the data to final.

--- script.R
+++ script.R
@@ -1,7 +1,12 @@
+# for One-Hot Encoding (OHE) 0,1
+install.packages("fastDummies")
+
 library(dplyr)
 library(tidyverse)
 library(readr)
 library(ggplot2)
+library(fastDummies)
+
 
 # import data set.
 cirrhosis <- read_csv("cirrhosis.csv")
@@ -15,7 +20,7 @@ ggplot(cirrhosis, aes(Age)) +
   theme_minimal()
 
 # check NA / missing value.
-colSums(is.na(cirrhosis))
+colMeans(is.na(cirrhosis)) * 100
 
 # review and summary the data.
 glimpse(cirrhosis)
@@ -28,6 +33,17 @@ view(cirrhosis_clean)
 # Imputed missing values with Mean all of numeric
 cirrhosis_imputed <- cirrhosis_clean %>% 
   mutate(across(where(is.numeric), ~replace_na(., mean(., na.rm = TRUE))))
+
+# one hot encoding
+names(cirrhosis)
+cirrhosis_final <- dummy_cols(cirrhosis_imputed, 
+                                select_columns = c("Sex", "Ascites", 
+                                                   "Hepatomegaly", "Spiders",
+                                                   "Edema"),
+                                remove_selected_columns = TRUE,
+                                remove_first_dummy = TRUE)
+
+glimpse(cirrhosis_final)
 
 
 
